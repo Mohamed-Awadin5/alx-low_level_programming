864583f436d368f3a4c14abdbdc131867626245a
Mohamed-Awadin5 <101401402+Mohamed-Awadin5@users.noreply.github.com>
2023-02-22 11:46:20 -0800

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include "main.h"
 /**
  * main - prints _putchar, fllowd by a new linees
@@ -17,23 +17,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-=======
-#include "main.h"
-/**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
- */
-int main(void)
-{
-	char abe[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
-	_putchar('\n');
-	return (0);
-}
->>>>>>> 873387ebc56e37486a0517d72f9030037e1ef562
