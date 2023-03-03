a525481991d76e09c0032fd32e4741a60c24378c
Mohamed.Awadin5 <mohamed.awadin6@gmail.com>
2023-03-03 12:23:00 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase exc. eq
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		{
+			if (c != 'e' && c != 'q')
+			{
+				putchar(c);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
