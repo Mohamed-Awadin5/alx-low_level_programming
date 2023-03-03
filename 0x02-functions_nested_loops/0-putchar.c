a3465cea33327fdfd92c693e090fa7f5d27f0cd3
Mohamed.Awadin5 <mohamed.awadin6@gmail.com>
2023-03-03 13:42:19 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,45 +1,21 @@
-<<<<<<< HEAD
-
-[A
-
-
-[A[D[D[D[D[A
 #include "main.h"
+
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main - print "_putchar" followed by new line
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
-=======
-#include "main.h"
-/**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
- */
-int main(void)
-{
-	char abe[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
->>>>>>> fe94d45a189535d1210680c6b348aa5f45b2be64
+}
